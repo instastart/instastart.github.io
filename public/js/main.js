a998ec1516eb469957041ac413123f997d51e8d6
hoa mai <hmai_23@yahoo.com>
2016-02-05 00:15:00 +0000

    Migranting our project

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,33 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
+$(document).ready(function(){
+  var count = 5;
+  add_task(); 
+  delete_task(); 
+  function add_task() {
 
-	$(".name-link").click(function() {
-		var oldText = $(this).find("h3").text();
-		$(this).find("h3").text(anagrammedName(oldText));
-	});
-})
+  $('.add-new-task').submit(function(){
+    var new_task = $('.add-new-task input[name=new-task]').val();
+    count = count + 1;
+    var print = '<li><span>' + new_task + '</span><img id="' + count + '" class="delete-button" width="10px" src="http://bit.ly/1JKyhh5" /></li>';
+    if(new_task !== ''){
+      $('.add-new-task input[name=new-task]').val('');
+      $(print).appendTo('.task-list ul').hide().fadeIn();
+      delete_task();
+    }
+    return false;
+    });
+  }
+
+  function delete_task() {
+    $('.delete-button').click(function(){
+      var current_element = $(this);
+      var id = $(this).attr('id');
+      current_element.parent().fadeOut("fast", function() { $(this).remove(); });
+    });
+  }
+});
 
 /*
  * Function that is called when the document is ready.
