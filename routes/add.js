56a8a2e4408c9b862f424aeb384550edad6c9c9c
Neil Sengupta <neil96sengupta@gmail.com>
2016-02-04 23:15:23 -0800

    scaffolding

--- routes/add.js
+++
@@ -1,12 +0,0 @@
-var data = require("../data.json");
-
-exports.addFriend = function(req, res) {    
-	// Your code goes here
-	var name = req.query.name;
-	var desc = req.query.description;
-	var obj = {"name": name, "description": desc};
-	console.log(obj);
-	data["friends"].push(obj);
-
-	res.render('addFriend', data);
- }
